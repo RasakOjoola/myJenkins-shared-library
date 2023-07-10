0353929f43e8c7e00724ed2522c165964440e21c
Rasak Ojoola <ojoolarasak@gmail.com>
2023-07-10 19:54:13 +0000

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 def call(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t rasakojoola/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push rasakojoola/my-repo:jma-2.0'
     }
 
 }
